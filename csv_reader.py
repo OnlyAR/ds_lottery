5ce36ff5eeee08c041e7ed979f23f3a1569c92b6
Your Name <you@example.com>
2023-01-09 19:18:34 +0800

    init project

---
+++ csv_reader.py
@@ -0,0 +1,13 @@
+import csv
+
+
+def get_result_map(filename):
+    with open(filename, 'r', encoding='utf8') as f:
+        reader = csv.reader(f)
+        idx = 0
+        result = {}
+        for row in reader:
+            if idx > 0:
+                result[row[0]] = float(row[1])
+            idx += 1
+        return result
